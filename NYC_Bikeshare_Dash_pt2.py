fb0b1f35689e2454e402178205233dd4b4f61705
Joseph Sharp <159794763+MojoS96@users.noreply.github.com>
2024-08-31 15:21:57 +0100

    Update NYC_Bikeshare_Dash_pt2.py

--- NYC_Bikeshare_Dash_pt2.py
+++ NYC_Bikeshare_Dash_pt2.py
@@ -1,4 +1,18 @@
 ################################################ NYC BIKES DASHBOARD #####################################################
+import subprocess
+import sys
+
+def install_requirements():
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements_nycbikes_venv.txt"])
+        print("All dependencies installed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while installing dependencies: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    install_requirements()
+
 import streamlit as st
 import pandas as pd
 import numpy as np
